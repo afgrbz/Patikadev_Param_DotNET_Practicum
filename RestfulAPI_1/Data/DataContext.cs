d1afb8ff0ac4896e6bbc99da19a4379f7dde5d56
afgrbz <99791289+afgrbz@users.noreply.github.com>
2023-02-10 16:45:58 +0300

    Proje dosyası ekle.

---
+++ RestfulAPI_1/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace RestfulAPI_1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=superherodb;Trusted_Connection=true;TrustServerCertificate=true;");
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
